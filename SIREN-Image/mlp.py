39d803978139658b59c03e74bd924572f56eff50
AbedSoleymani <zsoleymani@ualberta.ca>
2023-09-18 22:32:36 -0600

    MLP with ReLU nonlinearity for comparison with SIRON

---
+++ SIREN-Image/mlp.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, in_dim=2, hidden_dim=256, out_dim=1):
+        super(MLP, self).__init__()
+
+        self.net = nn.Sequential(nn.Linear(in_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, out_dim))
+
+    def forward(self, x):
+        return self.net(x)
