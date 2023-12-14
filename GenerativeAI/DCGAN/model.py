50f3893ad730f3b279fe71d8a889d7d0efbe0f6a
AbedSoleymani <zsoleymani@ualberta.ca>
2023-12-14 12:35:15 -0700

    adding generator class

--- GenerativeAI/DCGAN/model.py
+++ GenerativeAI/DCGAN/model.py
@@ -17,34 +17,34 @@ class Discriminator(nn.Module):
                       padding=1),
             nn.LeakyReLU(0.2),
             
-            #Input: N x features_d x 32 x 32
+            # Input: N x features_d x 32 x 32
             self._block(in_channels=features_d,
                         out_channels=features_d*2,
                         kernel_size=4,
                         stride=2,
                         padding=1),
             
-            #Input: N x features_d*2 x 16 x 16
+            # Input: N x features_d*2 x 16 x 16
             self._block(in_channels=features_d*2,
                         out_channels=features_d*4,
                         kernel_size=4,
                         stride=2,
                         padding=1),
             
-            #Input: N x features_d*4 x 8 x 8
+            # Input: N x features_d*4 x 8 x 8
             self._block(in_channels=features_d*4,
                         out_channels=features_d*8,
                         kernel_size=4,
                         stride=2,
                         padding=1),
             
-            #Input: N x features_d*8 x 4 x 4
+            # Input: N x features_d*8 x 4 x 4
             nn.Conv2d(in_channels=features_d*8,
                       out_channels=1,
                       kernel_size=4,
                       stride=2,
                       padding=0),
-            #Output: N x 1 x 1 x 1
+            # Output: N x 1 x 1 x 1
 
             nn.Sigmoid(),
         ),
@@ -65,4 +65,67 @@ class Discriminator(nn.Module):
     
     def forward(self, x):
         return self.disc(x)
+    
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            # Input: N × z_dim x 1 x 1
+            self._block(in_channels=z_dim,
+                        out_channels=features_g*16,
+                        kernel_size=4,
+                        stride=1,
+                        padding=0),
+
+            # Input: N x features_g*16 × 4 x 4
+            self._block(in_channels=features_g*16,
+                        out_channels=features_g*8,
+                        kernel_size=4,
+                        stride=2,
+                        padding=1),
+        
+            # Input: N x features_g*8 × 8 x 8
+            self._block(in_channels=features_g*8,
+                        out_channels=features_g*4,
+                        kernel_size=4,
+                        stride=2,
+                        padding=1),
+        
+            # Input: N x features_g*4 × 16 x 16
+            self._block(in_channels=features_g*4,
+                        out_channels=features_g*2,
+                        kernel_size=4,
+                        stride=2,
+                        padding=1),
+        
+            # Input: N x features_g*2 × 32 x 32
+            nn.ConvTranspose2d(
+                in_channels=features_g*2,
+                out_channels=channels_img,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+            ),
+
+            # Output: N x channels_img × 64 x 64
+            nn.Tanh(), # to map the output to [-1, 1]
+        ),
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                bias=False, # Since we are using BN
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+    
+    def forward(self, x):
+        return self.gen(x)
     
